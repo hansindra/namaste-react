6d019b8c2bef793675ffad875529120b66abac9f
Nadeera Rajapaksha <ndra@Nadeeras-MacBook-Air.local>
2024-02-11 15:34:22 +0530

    episode-01

---
+++ App.js
@@ -0,0 +1,8 @@
+const heading = React.createElement("h1",
+{
+    id:"heading"
+},
+"I am remembering everything I learn till my last breath");
+const rootForReact = ReactDOM.createRoot(document.getElementById("root"));
+
+rootForReact.render(heading);
